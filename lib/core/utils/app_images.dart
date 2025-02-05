97d2b270c882e969024b6ffd0f24540bdedf123f
Mazen Eldaly <dalydevelopment15@gmail.com>
2025-02-05 10:15:06 +0200

    Refactor and add utility files for app colors, images, and styles
    - Added `app_colors.dart` to define application color constants.
    - Added `app_images.dart` to manage image asset paths.
    - Added `app_styles.dart` to define responsive text styles.
    - Updated `main.dart` to use a new `CaffeineApp` class and removed default Flutter demo code.
    - Updated `pubspec.yaml` to include image assets and define custom fonts.
    - Removed `widget_test.dart` as it was a basic Flutter widget test.

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for imagesBean
+  /// assets/images/bean.svg
+  static const String imagesBean = "assets/images/bean.svg";
+
+  /// Assets for imagesCoffeeBk
+  /// assets/images/coffee_bk.png
+  static const String imagesCoffeeBk = "assets/images/coffee_bk.png";
+
+  /// Assets for imagesDeleivry
+  /// assets/images/deleivry.svg
+  static const String imagesDeleivry = "assets/images/deleivry.svg";
+
+  /// Assets for imagesMilk
+  /// assets/images/milk.svg
+  static const String imagesMilk = "assets/images/milk.svg";
+}
