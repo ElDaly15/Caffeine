532790800c59b9faab50c8656c4ac1998b67f7a6
Daly <dalydevelopment15@gmail.com>
2025-02-06 13:34:42 +0200

    Remove unused import
    Removed the unused import for `app_images` from the `social_media_widget_login_and_reg.dart` file to clean up the code.

--- lib/featuers/auth/presentation/views/widgets/social_media_widget_login_and_reg.dart
+++ lib/featuers/auth/presentation/views/widgets/social_media_widget_login_and_reg.dart
@@ -1,5 +1,4 @@
 import 'package:caffeine/core/utils/app_colors.dart';
-import 'package:caffeine/core/utils/app_images.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
