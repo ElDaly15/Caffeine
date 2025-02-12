ef00e4f9e427633fa2464a32a8e50c3d5337a17f
Daly <dalydevelopment15@gmail.com>
2025-02-12 12:19:38 +0200

    Remove ContainerOfProductDetails widget and update RowOfContainersOfProductDetails to display rating icon and value

--- lib/featuers/product/presentation/views/widgets/row_of_container_of_product_details.dart
+++ lib/featuers/product/presentation/views/widgets/row_of_container_of_product_details.dart
@@ -1,7 +1,7 @@
-import 'package:caffeine/core/utils/app_images.dart';
+import 'package:caffeine/core/utils/app_colors.dart';
 import 'package:caffeine/core/utils/app_styles.dart';
-import 'package:caffeine/featuers/product/presentation/views/widgets/container_of_product_details.dart';
 import 'package:flutter/material.dart';
+import 'package:iconly/iconly.dart';
 
 class RowOfContainersOfProductDetails extends StatelessWidget {
   const RowOfContainersOfProductDetails({super.key});
@@ -15,20 +15,22 @@ class RowOfContainersOfProductDetails extends StatelessWidget {
           style: TextStyles.font18Medium(context).copyWith(color: Colors.grey),
         ),
         Spacer(),
-        ContainerOfProductDetails(
-          image: Assets.imagesDeleivry,
-        ),
-        SizedBox(
-          width: 4,
-        ),
-        ContainerOfProductDetails(
-          image: Assets.imagesBean,
-        ),
-        SizedBox(
-          width: 4,
-        ),
-        ContainerOfProductDetails(
-          image: Assets.imagesMilk,
+        Row(
+          children: [
+            Icon(
+              IconlyBold.star,
+              color: AppColors.mainColorTheme,
+              size: 22,
+            ),
+            const SizedBox(width: 2),
+            Text(
+              '4.8',
+              style: TextStyles.font18Medium(context).copyWith(),
+            ),
+            SizedBox(
+              width: 6,
+            ),
+          ],
         ),
       ],
     );
