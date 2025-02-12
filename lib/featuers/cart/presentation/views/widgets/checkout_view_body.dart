51e64b53457e259bf136458e8986c0ec3e33464c
Daly <dalydevelopment15@gmail.com>
2025-02-12 17:33:32 +0200

    Add CustomBigElevatedBtmWithIcon button and update CheckoutViewBody for address handling

--- lib/featuers/cart/presentation/views/widgets/checkout_view_body.dart
+++ lib/featuers/cart/presentation/views/widgets/checkout_view_body.dart
@@ -1,11 +1,21 @@
+import 'package:caffeine/core/utils/app_colors.dart';
 import 'package:caffeine/core/utils/app_styles.dart';
+import 'package:caffeine/core/widgets/buttons/big_elevated_btm_with_icon.dart';
 import 'package:caffeine/core/widgets/headers/header_with_title_and_bk_btm.dart';
 import 'package:caffeine/featuers/cart/presentation/views/widgets/column_of_edit_address_and_add_note.dart';
 import 'package:caffeine/featuers/cart/presentation/views/widgets/deleiver_and_pick_up_check.dart';
 import 'package:flutter/material.dart';
+import 'package:iconly/iconly.dart';
 
-class CheckoutViewBody extends StatelessWidget {
+class CheckoutViewBody extends StatefulWidget {
   const CheckoutViewBody({super.key});
+
+  @override
+  State<CheckoutViewBody> createState() => _CheckoutViewBodyState();
+}
+
+class _CheckoutViewBodyState extends State<CheckoutViewBody> {
+  bool haveAddress = true;
 
   @override
   Widget build(BuildContext context) {
@@ -57,15 +67,26 @@ class CheckoutViewBody extends StatelessWidget {
             height: 10,
           ),
         ),
-        SliverPadding(
-          padding: const EdgeInsets.symmetric(horizontal: 22),
-          sliver: SliverToBoxAdapter(
-            child: ColumnOfAddressAndEditAndAddNote(
-              editAddress: () {},
-              addNote: () {},
-            ),
-          ),
-        ),
+        haveAddress
+            ? SliverPadding(
+                padding: const EdgeInsets.symmetric(horizontal: 22),
+                sliver: SliverToBoxAdapter(
+                  child: ColumnOfAddressAndEditAndAddNote(
+                    editAddress: () {},
+                    addNote: () {},
+                  ),
+                ),
+              )
+            : SliverPadding(
+                padding: const EdgeInsets.symmetric(horizontal: 22),
+                sliver: SliverToBoxAdapter(
+                  child: CustomBigElevatedBtmWithIcon(
+                    onPressed: () {},
+                    title: 'Add Address',
+                    iconData: IconlyBold.plus,
+                  ),
+                ),
+              )
       ],
     );
   }
