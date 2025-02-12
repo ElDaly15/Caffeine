d670752b943bce0d2e7694edc87f096b595851d2
Daly <dalydevelopment15@gmail.com>
2025-02-12 12:00:41 +0200

    Refactor CartViewBody to use Column layout and add ContainerOfTotalPriceAndProcessCheckOut for improved cart summary display

--- lib/featuers/cart/presentation/views/widgets/cart_view_body.dart
+++ lib/featuers/cart/presentation/views/widgets/cart_view_body.dart
@@ -1,41 +1,37 @@
 import 'package:caffeine/core/utils/app_colors.dart';
 import 'package:caffeine/core/utils/app_styles.dart';
 import 'package:caffeine/core/widgets/headers/custom_header_of_main_caffeine_app.dart';
+import 'package:caffeine/featuers/cart/presentation/views/widgets/container_of_total_price_and_checkout.dart';
 import 'package:caffeine/featuers/cart/presentation/views/widgets/sliver_list_of_cart_item.dart';
 import 'package:flutter/material.dart';
 
 class CartViewBody extends StatelessWidget {
   const CartViewBody({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      physics: ClampingScrollPhysics(),
-      slivers: [
-        SliverToBoxAdapter(
-          child: Container(
-            height: 10,
-            color: AppColors.mainColorTheme,
+    return Column(
+      children: [
+        Container(
+          height: 10,
+          color: AppColors.mainColorTheme,
+        ),
+        const CustomHeaderOfCaffeieApp(),
+        Expanded(
+          // This fixes the issue
+          child: CustomScrollView(
+            physics: const ClampingScrollPhysics(),
+            slivers: [
+              SliverToBoxAdapter(
+                  child: SizedBox(
+                height: 10,
+              )),
+              SliverListOfCartItems(),
+            ],
           ),
         ),
-        SliverToBoxAdapter(
-          child: CustomHeaderOfCaffeieApp(),
+        ContainerOfTotalPriceAndProcessCheckOut(
+          onPressed: () {},
         ),
-        SliverToBoxAdapter(
-          child: SizedBox(
-            height: 10,
-          ),
-        ),
-        SliverPadding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          sliver: SliverToBoxAdapter(
-            child: Text(
-              'My Cart',
-              style: TextStyles.font24SemiBold(context),
-            ),
-          ),
-        ),
-        SliverListOfCartItems(),
       ],
     );
   }
---
+++ lib/featuers/cart/presentation/views/widgets/container_of_total_price_and_checkout.dart
@@ -0,0 +1,69 @@
+import 'package:caffeine/core/utils/app_colors.dart';
+import 'package:caffeine/core/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+
+class ContainerOfTotalPriceAndProcessCheckOut extends StatelessWidget {
+  const ContainerOfTotalPriceAndProcessCheckOut(
+      {super.key, required this.onPressed});
+  final void Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      height: 115,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                'Total (3 item) : ',
+                style: TextStyles.font18SemiBold(context).copyWith(
+                  color: Colors.grey,
+                ),
+              ),
+              Spacer(),
+              Text(
+                r'$198.00',
+                style: TextStyles.font20Bold(context),
+              ),
+            ],
+          ),
+          Spacer(),
+          SizedBox(
+            height: 50,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.mainColorTheme,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  )),
+              onPressed: onPressed,
+              child: Row(
+                children: [
+                  Text(
+                    'Proceed to Checkout',
+                    style: TextStyles.font18SemiBold(context).copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8)),
+                    child: Icon(
+                      Icons.arrow_forward_ios,
+                      color: AppColors.mainColorTheme,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
