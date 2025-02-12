19da6f5c4b94e4095dfdce037124fb281b01d62d
Daly <dalydevelopment15@gmail.com>
2025-02-12 11:29:36 +0200

    Add swipeable cart item container and integrate counter functionality

---
+++ lib/featuers/cart/presentation/views/widgets/container_of_cart_item.dart
@@ -0,0 +1,112 @@
+import 'package:caffeine/core/utils/app_colors.dart';
+import 'package:caffeine/core/utils/app_images.dart';
+import 'package:caffeine/core/utils/app_styles.dart';
+import 'package:caffeine/core/widgets/buttons/inc_and_dec_counter_of_cart.dart';
+import 'package:flutter/material.dart';
+import 'package:swipeable_tile/swipeable_tile.dart';
+
+class ContainerOfCartItem extends StatefulWidget {
+  const ContainerOfCartItem(
+      {super.key, required this.onSwiped, required this.counterOfProduct});
+  final void Function(SwipeDirection) onSwiped;
+  final void Function(int) counterOfProduct;
+  @override
+  State<ContainerOfCartItem> createState() => _ContainerOfCartItemState();
+}
+
+class _ContainerOfCartItemState extends State<ContainerOfCartItem> {
+  @override
+  Widget build(BuildContext context) {
+    return SwipeableTile.card(
+      color: Colors.white,
+      borderRadius: 12,
+      shadow: BoxShadow(
+        // ignore: deprecated_member_use
+        color: Colors.grey.withOpacity(0.3),
+        spreadRadius: 2,
+        blurRadius: 2,
+        offset: const Offset(0, 1), // changes position of shadow
+      ),
+      horizontalPadding: 0,
+      verticalPadding: 12,
+      direction: SwipeDirection.endToStart,
+      onSwiped: widget.onSwiped,
+      backgroundBuilder: (context, direction, progress) {
+        return AnimatedBuilder(
+          animation: progress,
+          builder: (context, child) {
+            return AnimatedContainer(
+              duration: const Duration(milliseconds: 400),
+              color: progress.value > 0.4
+                  ? AppColors.mainColorTheme
+                  : const Color(0xFFed7474),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Icon(
+                    Icons.delete,
+                    color: AppColors.secondaryColorTheme,
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      },
+      key: UniqueKey(),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: IntrinsicHeight(
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.asset(
+                  Assets.imagesLatte,
+                  scale: 2.5,
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Latte Cappuccino',
+                      style: TextStyles.font20SemiBold(context),
+                    ),
+                    Text(
+                      'Hot/Cold',
+                      style: TextStyles.font18Medium(context)
+                          .copyWith(color: Colors.grey),
+                    ),
+                    Spacer(),
+                    Row(
+                      children: [
+                        Text(
+                          r'$198.00',
+                          style: TextStyles.font20Bold(context),
+                        ),
+                        Spacer(),
+                        IncreaseAndDecreaseContainer(
+                          counterOfProduct: widget.counterOfProduct,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
